df02874c67f941c8ae178b6b9009eb55a10a497d
Rafal Jasinski <rajs@softwaremind.com>
2025-11-05 12:37:47 +0100

    Fixed activation

--- conda-setup.sh
+++ conda-setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 yes | conda create --prefix=.venv python=3.9
-conda activate .venv
+conda activate .venv/
 conda install "ffmpeg<5" -c conda-forge
 python -m pip install 'torch==2.1.0'
 python -m pip install setuptools wheel
