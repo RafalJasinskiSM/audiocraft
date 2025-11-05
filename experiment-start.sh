089a5bda2cf719caa3e7a1acaa02299beb4cf60c
Rafal Jasinski <rajs@softwaremind.com>
2025-11-05 12:57:26 +0100

    Fixed experiment start script

--- experiment-start.sh
+++ experiment-start.sh
@@ -1,6 +1,6 @@
 # Check if three arguments are provided
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 experiment_name experinent/config dset/config"
+    echo "Usage: $0 experiment_name solver/config dset/config"
     exit 1
 fi
 
@@ -8,5 +8,5 @@ fi
 conda env config vars set AUDIOCRAFT_DORA_DIR=$1
 conda deactivate
 conda activate .venv/
-dora run $2 $3
+dora run solver=$2 dset=$3
 
